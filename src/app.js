2569e1516ad3bff42c4030ce9073ead6e9bff41b
John <user@johnpoosmacbook.home>
2020-02-19 00:16:35 +0000

    mvp complete yo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#app',
     data:{
       ratesData: {},
-      selectedCurrency: 0
+      selectedCurrency: 1,
+      amountToExchange: 1,
+      selectedCurrency2: 1,
+      amountToExchange2: 1
 
     },
     mounted(){
@@ -13,8 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     },
     computed:{
+      euroToOther: function() {
+        return (this.amountToExchange * this.selectedCurrency).toFixed(2)},
 
-    },
+        otherToEuro: function () {
+          return (this.amountToExchange2 / this.selectedCurrency2).toFixed(2)}
+
+      },
     methods:{
       fetchExchange: function(){
         const request = fetch("https://api.exchangeratesapi.io/latest")
